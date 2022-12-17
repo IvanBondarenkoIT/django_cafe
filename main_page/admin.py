d15a71090467961c90add3b997bec666c4035193
Ivan <ivan.bondarenko.it@gmail.com>
2022-12-17 14:13:01 +0400

    Migrate DB Create User:admin Fill DB

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import DishCategory, Dish, About, WhyUs, Events
+
 
 # Register your models here.
+admin.site.register(Dish)
+admin.site.register(DishCategory)
+admin.site.register(About)
+admin.site.register(WhyUs)
+admin.site.register(Events)
--- main_page/models.py
+++ main_page/models.py
@@ -13,9 +13,13 @@ class About(models.Model):
         return os.path.join('images/dishes', filename)
 
     title = models.CharField(max_length=100, unique=True)
-    desc_body = models.TextField(max_length=400, blank=True) # Можливо висточить одного опису
+    desc_body = models.TextField(max_length=600, blank=True) # Можливо висточить одного опису
     desc_bottom = models.TextField(max_length=400, blank=True) # Якщо не вистачить
     video = models.FileField(upload_to=get_file_name)
+
+    def __str__(self):
+        return f'{self.title}'
+
 
 
 # Why Us
@@ -25,12 +29,24 @@ class WhyUs(models.Model):
     position = models.PositiveSmallIntegerField()
     is_visible = models.BooleanField(default=True)
 
+    def __str__(self):
+        return f'{self.name}: {self.position}'
+
+    class Meta:
+        ordering = ('position', )
+
 
 # Menu
 class DishCategory(models.Model):
     name = models.CharField(max_length=50, unique=True)
     position = models.PositiveSmallIntegerField(unique=True)
     is_visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.name}: {self.position}'
+
+    class Meta:
+        ordering = ('position', )
 
 
 class Dish(models.Model):
@@ -49,6 +65,12 @@ class Dish(models.Model):
     ingredients = models.CharField(max_length=100)
     photo = models.ImageField(upload_to=get_file_name)
 
+    def __str__(self):
+        return f'{self.name}: {self.position}'
+
+    class Meta:
+        ordering = ('position', )
+
 
 # Events
 class Events(models.Model):
@@ -57,9 +79,15 @@ class Events(models.Model):
         filename = f'{uuid.uuid4()}.{ext}'
         return os.path.join('images/dishes', filename)
     name = models.CharField(max_length=50, unique=True)
-    desc = models.TextField(max_length=400, blank=True)
+    desc = models.TextField(max_length=800, blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     photo = models.ImageField(upload_to=get_file_name)
     is_visible = models.BooleanField(default=True)
     date = models.DateField()
     time = models.TimeField()
+
+    def __str__(self):
+        return f'{self.name}: {self.date}'
+
+    class Meta:
+        ordering = ('date', )
