8ce2bc980dd6d885381249d278febde21d65d0d2
Ivan <ivan.bondarenko.it@gmail.com>
2022-12-19 15:48:03 +0400

    HTML Decomposition

--- main_page/views.py
+++ main_page/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import DishCategory, Dish, About, WhyUs, Events
+from django.http import HttpResponse
+
 
 # Create your views here.
+def main_page(request):
+    categories = DishCategory.objects.filter(is_visible=True)
+    dishes = Dish.objects.filter(is_visible=True, is_special=False)
+    return render(request, 'index.html')
+    # return HttpResponse('\n'.join(map(str, categories)) + '\n'.join(map(str, dishes)))
