b759be7bed9002e21e5fa86ad14efae6b3138b4f
Ivan <ivan.bondarenko.it@gmail.com>
2022-12-20 14:01:29 +0400

    menu.html (done) spacials.html (done)

--- main_page/views.py
+++ main_page/views.py
@@ -5,7 +5,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 def main_page(request):
+    about_us = About.objects.all()
+    why_us = WhyUs.objects.filter(is_visible=True)
     categories = DishCategory.objects.filter(is_visible=True)
     dishes = Dish.objects.filter(is_visible=True, is_special=False)
-    return render(request, 'index.html')
+    special_dishes = Dish.objects.filter(is_visible=True, is_special=True)
+    events = Events.objects.filter(is_visible=True)
+    return render(request, 'main_page.html', context={
+        'categories': categories,
+        'dishes': dishes,
+        'about_us': about_us,
+        'why_us': why_us,
+        'specials': special_dishes,
+        'events': events,
+    })
     # return HttpResponse('\n'.join(map(str, categories)) + '\n'.join(map(str, dishes)))
