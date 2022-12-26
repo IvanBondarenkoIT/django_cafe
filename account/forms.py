a698fffb7c2392727bedeb2dd6a16d50ee7c0b5c
Ivan <ivan.bondarenko.it@gmail.com>
2022-12-26 15:48:39 +0400

    accounts (add) registration (done) login/logout (done)

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,22 @@
 from django import forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 
 User = get_user_model()
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput())
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user or not user.check_password(password):
+                raise forms.ValueError('Login or password is incorrect!')
+        return super().clean()
 
 
 class RegistrationForm(forms.ModelForm):
