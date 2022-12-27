3f821494d754350f29a5ae5853b62eb89e6f2047
Ivan <ivan.bondarenko.it@gmail.com>
2022-12-27 11:04:10 +0400

    all lessons (done)

--- manager/urls.py
+++ manager/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import reservations
+from .views import reservations, update_reservation
+
+app_name = 'manager'
 
 urlpatterns = [
     path('reservations/', reservations, name='reservations'),
+    path('reservations/update/<int:pk>/', update_reservation, name='update_reservation'),
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from main_page.models import UserReservation
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 
 # Create your views here.
+def is_manager(user):
+    return user.groups.filter(name='manager').exists()
+
+
 @login_required(login_url='/login/')
+@user_passes_test(is_manager)
 def reservations(request):
     messages = UserReservation.objects.filter(is_processed=False)
     return render(request, 'reservations.html', context={'reservations': messages})
+
+
+@login_required(login_url='/login/')
+@user_passes_test(is_manager)
+def update_reservation(request, pk):
+    UserReservation.objects.filter(pk=pk).update(is_processed=True)
+    return redirect('manager:reservations')
